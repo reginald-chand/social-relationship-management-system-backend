6ce012c1200e9faf58027157659b694620cfe31e
Reginald Chand <reginald-chand@outlook.com>
2024-06-26 00:54:38 +1200

    Sort imports

--- server/routes/routes.mjs
+++ server/routes/routes.mjs
@@ -1,7 +1,6 @@
+import { connectionController } from "../controllers/connection/connection.controller.mjs";
 import { csrfController } from "../controllers/auth/csrf.controller.mjs";
 import express from "express";
-
-import { connectionController } from "../controllers/connection/connection.controller.mjs";
 import { userVerificationMiddleware } from "../middlewares/user.verification.middleware.mjs";
 
 export const routes = express.Router();
