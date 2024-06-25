618fb2dba3e64b6fb4f0469793e1d5329b36a606
Reginald Chand <reginald-chand@outlook.com>
2024-06-26 00:49:34 +1200

    Create connection.controller.mjs

---
+++ server/controllers/connection/connection.controller.mjs
@@ -0,0 +1,89 @@
+import { ConnectionModel } from "../../models/connection/connection.model.mjs";
+import { connectionControllerValidator } from "../../validators/connection/connection.controller.validator.mjs";
+import { logger } from "../../configs/logger.config.mjs";
+import mongoose from "mongoose";
+
+export const connectionController = async (request, response) => {
+  const { error, value } = connectionControllerValidator.validate(request.body);
+
+  if (error) {
+    return response.status(400).json({ responseMessage: error.message });
+  }
+
+  const { userName, userData } = value;
+
+  try {
+    let foundFollowedUser = false;
+
+    const database = mongoose.connection.db;
+
+    const existingUser = await database
+      .collection("users")
+      .findOne({ userName: { $eq: userName } });
+
+    if (!existingUser) {
+      return response.status(404).json({ responseMessage: "User not found." });
+    }
+
+    const existingFollowings = await ConnectionModel.aggregate([
+      { $match: { "followings.userName": userName } },
+      { $unwind: "$followings" },
+      { $match: { "followings.userName": userName } },
+      { $project: { userName: userName } },
+    ]);
+
+    existingFollowings.forEach((followedUser) => {
+      if (userName === followedUser.userName) {
+        foundFollowedUser = true;
+      }
+    });
+
+    if (foundFollowedUser) {
+      await ConnectionModel.findOneAndUpdate(
+        { _id: { $eq: userData.id } },
+        { $pull: { followings: { userName: userName } } },
+        { new: true, upsert: true }
+      );
+
+      await database
+        .collection("followers")
+        .findOneAndUpdate(
+          { _id: { $eq: existingUser._id } },
+          { $pull: { followers: { userName: userData.userName } } },
+          { new: true, upsert: true }
+        );
+
+      return response
+        .status(200)
+        .json({ responseMessage: `Successfully un-followed ${userName}` });
+    }
+
+    await ConnectionModel.findOneAndUpdate(
+      { _id: { $eq: userData.id } },
+      { $push: { followings: { userName: userName } } },
+      { new: true, upsert: true }
+    );
+
+    await database
+      .collection("followers")
+      .findOneAndUpdate(
+        { _id: { $eq: existingUser._id } },
+        { $push: { followers: { userName: userData.userName } } },
+        { new: true, upsert: true }
+      );
+
+    return response
+      .status(200)
+      .json({ responseMessage: `Successfully followed ${userName}` });
+  } catch (error) {
+    logger.log({
+      level: "error",
+      message: error,
+      additional: "Internal server error.",
+    });
+
+    return response.status(500).json({
+      responseMessage: "Internal server error.",
+    });
+  }
+};
