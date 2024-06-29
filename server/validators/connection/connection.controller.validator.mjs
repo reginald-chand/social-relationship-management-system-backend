ac4870768d6a6cd2b1c739f96d5ada4dde8e61bf
Reginald Chand <reginald-chand@outlook.com>
2024-06-30 11:54:51 +1200

    Update csrf name

--- server/validators/connection/connection.controller.validator.mjs
+++ server/validators/connection/connection.controller.validator.mjs
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 export const connectionControllerValidator = Joi.object({
-  _csrf: Joi.string().required(),
+  csrfToken: Joi.string().required(),
 
   userName: Joi.string().pattern(new RegExp("^[a-z]+$")).required(),
 
